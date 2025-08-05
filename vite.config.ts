1ae53dcebf00868d2f479550c2af0f0dbacd9deb
Humberto Antonio Herrador Reyes <47198640+wbeto@users.noreply.github.com>
2025-08-05 13:11:57 -0600

    feat: configure alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import fs from 'fs'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -42,4 +44,19 @@ export default defineConfig({
       }
     })
   ],
+  preview: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, './certs/localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, './certs/localhost.pem'))
+    },
+    port: 4173
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Alias para la carpeta src
+      '@components': path.resolve(__dirname, './src/shared/components'),
+      '@utils': path.resolve(__dirname, './src/shared/utils'),
+      '@hooks': path.resolve(__dirname, './src/shared/hooks') 
+    } 
+  }
 })
